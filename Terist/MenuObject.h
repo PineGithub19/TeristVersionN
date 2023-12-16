83f0a36bb3dc449b4bd2c7bda3be2d3b9c08d1c8
thaihuyentung04@gmail.com <thaihuyentung04@gmail.com>
2023-12-16 11:55:32 +0700

    Comment MenuObject.h

--- Terist/MenuObject.h
+++ Terist/MenuObject.h
@@ -14,14 +14,14 @@ public:
 	MenuObject(const double& = 0, const bool& = false, const bool& = false);
 	~MenuObject();
 
-	int Main_Menu(SDL_Renderer*, SDL_Event, std::vector<TextObject*>&, MenuObject*&, bool&, int&);
-	void Move_Menu(SDL_Renderer*, BaseObject&);
+	int Main_Menu(SDL_Renderer*, SDL_Event, std::vector<TextObject*>&, MenuObject*&, bool&, int&); // Thuc hien hien thi menu chinh
+	void Move_Menu(SDL_Renderer*, BaseObject&); // Di chuyen hinh nen menu
 
-	bool LoadLogoGame(SDL_Renderer*, BaseObject&);
+	bool LoadLogoGame(SDL_Renderer*, BaseObject&); // Hien thi Logo nhom
 
-	void InitTitleGame(TextObject&, SDL_Renderer*, TTF_Font*);
-	void InitPlayGame(TextObject&, SDL_Renderer*, TTF_Font*);
-	void InitCreditsGame(TextObject&, SDL_Renderer*, TTF_Font*);
-	void InitQuitGame(TextObject&, SDL_Renderer*, TTF_Font*);
-	void InitSettingGame(TextObject&, SDL_Renderer*, TTF_Font*);
+	void InitTitleGame(TextObject&, SDL_Renderer*, TTF_Font*); // Hien thi ten game
+	void InitPlayGame(TextObject&, SDL_Renderer*, TTF_Font*); // Hien thi chu playgame
+	void InitCreditsGame(TextObject&, SDL_Renderer*, TTF_Font*); // Hien thi chu credits
+	void InitQuitGame(TextObject&, SDL_Renderer*, TTF_Font*); // Hien thi chu quitgame
+	void InitSettingGame(TextObject&, SDL_Renderer*, TTF_Font*); // hien thi chu setting
 };
