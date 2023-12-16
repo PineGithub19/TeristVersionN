64ad040a64257b7c7636fa9a3c7ba4a6e5544d95
thaihuyentung04@gmail.com <thaihuyentung04@gmail.com>
2023-12-16 11:54:55 +0700

    Comment CreditsObject.h

--- Terist/CreditsObject.h
+++ Terist/CreditsObject.h
@@ -9,6 +9,6 @@ public:
 	CreditsObject();
 	~CreditsObject();
 
-	void InitTextContent(SDL_Renderer*, TTF_Font*, TextObject&, const string&, const int&);
-	void LoadCredits(SDL_Renderer*, SDL_Event&, TTF_Font*, TTF_Font*, bool&, bool&);
+	void InitTextContent(SDL_Renderer*, TTF_Font*, TextObject&, const string&, const int&); // Render noi dung cua chu trong Credits
+	void LoadCredits(SDL_Renderer*, SDL_Event&, TTF_Font*, TTF_Font*, bool&, bool&); // Thuc hien hien thi phan Credits len man hinh chinh
 };
